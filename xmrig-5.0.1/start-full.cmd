c0fe6cff970546d6c75255048cbf7cda39a80159
MinecraftFuns <45290401+vmlankub@users.noreply.github.com>
2019-11-30 09:54:26 +0800

    Add files via upload

---
+++ xmrig-5.0.1/start-full.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o xmr.pool.minergate.com:45700 -u minecraftfuns@outlook.com -p x -k --coin monero
+pause
---
+++ xmrig-5.0.1/start-id-full.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o xmr.pool.minergate.com:45700 -u sh3p4dtf36gnajn -p x -k --coin monero
+pause
